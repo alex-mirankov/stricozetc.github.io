41175991732a049014bda4e611a7936c0acfbd0e
Aliaksandr Mirankou <AliaksandrMirankou@greensky.com>
2020-01-19 01:27:11 +0300

    deplu

---
+++ precache-manifest.fc81e3963f1f89b61dd4646aff181c24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "573057c424e404bc83e8",
+    "url": "/static/css/main.f4921b6d.chunk.css"
+  },
+  {
+    "revision": "573057c424e404bc83e8",
+    "url": "/static/js/main.573057c4.chunk.js"
+  },
+  {
+    "revision": "5dd31348e8565cd6aeef",
+    "url": "/static/js/1.5dd31348.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bbb60ba5f574a6023985b288d556f4ec",
+    "url": "/index.html"
+  }
+];
